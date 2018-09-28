2564f3a4ea6523fd6b34367e8964fb74fce65e46
Kira Hartlage <kira.hartlage@gmail.com>
2018-09-28 12:51:04 -0400

    can serialize data from json file

---
+++ favorite-episode/favorite-episode/Episode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FavoriteEpisode
+{
+    public class RootObject
+    {
+        public Episode[] Episode { get; set;  }
+    }
+
+    public class Episode
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string EpisodeName { get; set; }
+        [JsonProperty(PropertyName = "season")]
+        public string Season { get; set; }
+        [JsonProperty(PropertyName = "number")]
+        public string EpisodeNumber { get; set; }
+        [JsonProperty(PropertyName = "airdate")]
+        public string AirDate { get; set; }
+        [JsonProperty(PropertyName = "airtime")]
+        public string AirTime { get; set; }
+        [JsonProperty(PropertyName = "summary")]
+        public string Summary { get; set; }
+    }
+}
--- favorite-episode/favorite-episode/Program.cs
+++ favorite-episode/favorite-episode/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace favoriteepisode
+namespace FavoriteEpisode
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Just hanging");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "gilmoregirls.json");
+            var episodes = DeserializeEpisodes(fileName);
+
+            foreach(var episode in episodes)
+            {
+                Console.WriteLine("Season " + episode.Season + " Episode " + episode.EpisodeNumber + ": Episode name: " + episode.EpisodeName);
+            }
+        }
+
+        public static List<Episode> DeserializeEpisodes(string fileName)
+        {
+            var episodes = new List<Episode>();
+            var serializer = new JsonSerializer();
+            using(var reader = new StreamReader(fileName))
+            using(var jsonReader = new JsonTextReader(reader))
+            {
+                episodes = serializer.Deserialize<List<Episode>>(jsonReader);
+            }
+            return episodes;
         }
     }
 }
